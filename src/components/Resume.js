c07eb71d808df2b24a4efb5b5427819b2eb380ea
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2023-03-13 09:05:36 -0400

    Got rid of resume link.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,9 +2,7 @@ export default function Resume() {
   return (
     <div>
       <h2>Resume</h2>
-      <section>
-        <a href="../components/resume.pdf">Download resume</a>
-      </section>
+      <section>Resume furnished upon request.</section>
     </div>
   );
 }
